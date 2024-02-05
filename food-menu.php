3eb27f56c931395d9ca1e1e990d66dadd7d9d94b
AlfredKPK <alfredkong4school@gmail.com>
2024-02-05 15:18:58 +0800

    Home page update

--- food-menu.php
+++ food-menu.php
@@ -9,10 +9,9 @@ include_once "header.php";
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width,initial-scale=1.0">
             <link href="css/style.css" rel="stylesheet">
-            <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     </head>
     <body>
           <div class="largescreen">
-          
+            
           </div>
     </body>
--- index.php
+++ index.php
@@ -4,23 +4,28 @@ include_once "header.php";
 
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Hello!</title>
-        <meta charset="UTF-8">
-        <link rel="stylesheet" href="images/background.jpg">
-        <link href="css/style.css" rel="stylesheet">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-    </head>
 
-    <body>
-        <header class="home" id="home">
-            <div class="time-open">
-              <span class="large">Open from 10am to 12pm</span>
-            </div>
-            <div class="center">
-              <span class="small" style="font-size:100px">Pizza<br>Pasta</span>
-              <span class="medium" style="font-size:60px"><b><br>Spaghetti</b></span>
-            </div>
-          </header>
-    </body>
+<head>
+  <title>Hello!</title>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="images/background.jpg">
+  <link href="css/style.css" rel="stylesheet">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+  <div class="largescreen">
+  <h1 style="text-align:center;margin-top: -10%;padding:50px;">Welcome to ______ Restaurant</h3>
+    <div class="side-by-side">
+        <p>Welcome to ____ Restaurant, in here, we offer Italic and Japanese food. <br><br>
+        The resturant is open from 10am to 12pm every day. <br><br>
+        We aim to provide premium food at a reasonable price!
+        </p>
+        <div class="pictures">
+          <img align="right" src="images/homepicture.png" alt="Pizza" style="width:300px;height:250px" ;>
+        </div>
+    </div>
+  </div>
+</body>
+
 </html>
